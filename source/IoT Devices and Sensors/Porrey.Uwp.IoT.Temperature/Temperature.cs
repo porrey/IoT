654fb4009a73c33b29d9d8a165a211b745de63b7
Daniel Porrey <daniel.porrey@hotmail.com>
2015-10-15 22:30:09 -0500

    Added temperature project.

---
+++ source/IoT Devices and Sensors/Porrey.Uwp.IoT.Temperature/Temperature.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2015 Daniel Porrey
+//
+// This file is part of the Sensor Telemetry solution.
+namespace System
+{
+	/// <summary>
+	/// Helper methods for temperature conversion
+	/// </summary>
+	public class Temperature
+	{
+		/// <summary>
+		/// Converts a temperature in Celsius to Fahrenheit.
+		/// </summary>
+		/// <param name="fahrenheit">The temperature in Celsius.</param>
+		/// <returns>Returns the temperature in Fahrenheit.</returns>
+		public static float ConvertToCelsius(float fahrenheit) => (fahrenheit - 32f) * 5f / 9f;
+
+		/// <summary>
+		/// Converts a temperature in Fahrenheit to Celsius.
+		/// </summary>
+		/// <param name="celsius">The temperature in Fahrenheit.</param>
+		/// <returns>Returns the temperature in Celsius.</returns>
+		public static float ConvertToFahrenheit(float celsius) => (celsius * 9f / 5f) + 32f;
+	}
+}
