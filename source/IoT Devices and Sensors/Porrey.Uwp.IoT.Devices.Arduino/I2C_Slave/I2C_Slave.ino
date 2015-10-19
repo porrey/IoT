1f4281d2a74b158bc3aa25c91699e66ef01924c4
Daniel Porrey <daniel.porrey@hotmail.com>
2015-10-18 22:37:41 -0500

    Added licensing information.

--- source/IoT Devices and Sensors/Porrey.Uwp.IoT.Devices.Arduino/I2C_Slave/I2C_Slave.ino
+++ source/IoT Devices and Sensors/Porrey.Uwp.IoT.Devices.Arduino/I2C_Slave/I2C_Slave.ino
@@ -1,3 +1,22 @@
+// Copyright © 2015 Daniel Porrey. All Rights Reserved.
+//
+// This file is part of the IoT Devices and Sensors project.
+// 
+// IoT Devices and Sensors is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// IoT Devices and Sensors is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with IoT Devices and Sensors. If not, 
+// see http://www.gnu.org/licenses/.
+//
+
 /*
   Arduino i2c Slave
   written by Daniel Porrey
@@ -11,6 +30,7 @@
 
   The C# library is available in NuGet; ID = IoT.Arduino
 */
+
 #include <Wire.h>
 
 // ***
